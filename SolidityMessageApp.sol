a63adcbd39b4e283b677020debf6a31af9bef564
lost brain404 <lostbrain404@gmail.com>
2025-05-04 00:14:50 +0100

    Create SolidityMessageApp.sol

---
+++ SolidityMessageApp.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract MessageStorage {
+    address public Owner ;
+    string public Message ;
+    string [] public MessageHistory ;
+
+    event MessageUpdated(string OldMsg , string NewMsg);
+
+    ///@notice the onwer 
+    constructor (){
+        Owner = msg.sender ;
+    }
+
+    modifier OnlyOnwer(){
+        require(msg.sender == Owner, "Only the owner can update the message"); _;
+    }
+
+    function SetMessage(string memory _InitialMemory)public OnlyOnwer{
+        string memory Old = _InitialMemory ;
+        MessageHistory.push(_InitialMemory);
+        emit MessageUpdated(Old,_InitialMemory);
+    }
+
+    function GetMessage()public view returns (string memory){
+        return Message;
+    }
+
+}
