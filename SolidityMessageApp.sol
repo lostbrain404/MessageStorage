6c239c3124523ba189a020855c0c43e1bc0883bb
lost brain404 <lostbrain404@gmail.com>
2025-05-04 16:00:29 +0100

    Update SolidityMessageApp.sol

--- SolidityMessageApp.sol
+++ SolidityMessageApp.sol
@@ -17,12 +17,14 @@ contract MessageStorage {
         require(msg.sender == Owner, "Only the owner can update the message"); _;
     }
 
+    ///@notice update the message & register it 
     function SetMessage(string memory _InitialMemory)public OnlyOnwer{
         string memory Old = _InitialMemory ;
         MessageHistory.push(_InitialMemory);
         emit MessageUpdated(Old,_InitialMemory);
     }
 
+    ///@notice get the new message 
     function GetMessage()public view returns (string memory){
         return Message;
     }
